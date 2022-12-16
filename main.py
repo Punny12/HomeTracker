ca342553e3afb0099cd7f8b032cea36b773f40d3
Panni <pranavpanni12@gmail.com>
2022-12-16 15:52:09 +0530

    Daily commit

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.properties import StringProperty
+from kivy.lang import Builder
 
 
-class MyApp(App):
+class HomePage(Screen):
+    pass
+
+
+class TransactionPage(Screen):
+    pass
+
+
+class SettingsPage(Screen):
+    pass
+
+
+class IndexPage(BoxLayout):
+    label_to_update = StringProperty("")
+
+    def display_home_page(self):
+        w = HomePage()
+        self.add_widget(w)
+
+    def display_transaction_page(self):
+        self.label_to_update = "Trans"
+
+    def display_settings_page(self):
+        self.label_to_update = "sett"
+
+
+
+class IndexApp(App):
+    Window.size = (350, 650)
+    Window.background_color: (1, 1, 1, 1)
+    kv_directory = "templates"
+
     def build(self):
-        return Label(text='Hello world')
+        return IndexPage()
 
 
 if __name__ == "__main__":
-    MyApp().run()
+    IndexApp().run()
